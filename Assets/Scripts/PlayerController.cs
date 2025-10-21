41ddb898cf1fcd5db120a3b0cc4bd41cf0d66e22
Ylmazzkk <ylmazz145@gmail.com>
2025-10-22 01:01:01 +0300

    Add files via upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof (BoxCollider))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private FixedJoystick _joystick;
+    [SerializeField] private Animator _animator;
+
+    [SerializeField] private float _moveSpeed;
+
+    private void FixedUpdate()
+    {
+        _rigidbody.linearVelocity = new Vector3(_joystick.Horizontal * _moveSpeed, _rigidbody.linearVelocity.y, _joystick.Vertical * _moveSpeed);
+        
+        if (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(_rigidbody.linearVelocity);
+        }
+    }
+}
